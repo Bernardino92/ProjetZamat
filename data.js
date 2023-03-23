6203b0fb84732549d685b5634ea41c0556240e93
Bernardino92 <bernardino1992@gmail.com>
2023-03-23 18:27:58 +0000

    Commit

--- data.js
+++ data.js
@@ -33,191 +33,19 @@ var APP_DATA = {
           "yaw": -0.6308191941445092,
           "pitch": -0.3338410633027582,
           "rotation": 1.5707963267948966,
-          "target": "1-salon"
+          "target": "4-appartement---salon"
         },
         {
-          "yaw": 0.12788410075394374,
-          "pitch": 0.4325246940779639,
+          "yaw": 0.1278845111999889,
+          "pitch": 0.4261991138584822,
           "rotation": 0,
-          "target": "4-garage"
+          "target": "5-garage"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-salon",
-      "name": "Salon",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": -0.05558689878761136,
-        "pitch": -0.01023984023624358,
-        "fov": 1.5318800993415687
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.07226719253980463,
-          "pitch": 0.01789412698462911,
-          "rotation": 4.71238898038469,
-          "target": "3-salle-de-bain"
-        },
-        {
-          "yaw": 0.17388843313741376,
-          "pitch": 0.1708287058711182,
-          "rotation": 0,
-          "target": "2-chambre"
-        },
-        {
-          "yaw": -2.7989711427221753,
-          "pitch": 0.14192243091712875,
-          "rotation": 0,
-          "target": "0-faade-avant"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-chambre",
-      "name": "Chambre",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.5318800993415687
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.1629850655826246,
-          "pitch": 0.15643085284192537,
-          "rotation": 0,
-          "target": "1-salon"
-        },
-        {
-          "yaw": -2.0258679696182575,
-          "pitch": 0.17788284589516934,
-          "rotation": 1.5707963267948966,
-          "target": "3-salle-de-bain"
-        },
-        {
-          "yaw": 1.1459649446804185,
-          "pitch": 0.11858724172162027,
-          "rotation": 0,
-          "target": "5-faade-derrier"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-salle-de-bain",
-      "name": "Salle De Bain",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.071950949715042,
-          "pitch": 0.07861267624546997,
-          "rotation": 7.853981633974483,
-          "target": "1-salon"
-        },
-        {
-          "yaw": 2.347889908997061,
-          "pitch": 0.04316314932021825,
-          "rotation": 4.71238898038469,
-          "target": "2-chambre"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-garage",
-      "name": "Garage",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.38374366416717365,
-          "pitch": 0.028608292918884004,
-          "rotation": 0,
-          "target": "1-salon"
-        },
-        {
-          "yaw": -1.9363189010425153,
-          "pitch": 0.013458224380940464,
-          "rotation": 4.71238898038469,
-          "target": "0-faade-avant"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-faade-derrier",
+      "id": "1-faade-derrier",
       "name": "Façade Derrier",
       "levels": [
         {
@@ -249,13 +77,201 @@ var APP_DATA = {
           "yaw": 0.5341431309070117,
           "pitch": -0.30856213095555596,
           "rotation": 4.71238898038469,
-          "target": "2-chambre"
+          "target": "3-appartement---chambre"
         },
         {
-          "yaw": -0.10505230207259508,
-          "pitch": 0.48489855606763754,
+          "yaw": -0.10505250839501734,
+          "pitch": 0.4848989182749808,
           "rotation": 0,
-          "target": "4-garage"
+          "target": "5-garage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-appartement---salle-de-bain",
+      "name": "Appartement - Salle De Bain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3612317331374735,
+          "pitch": 0.2261013739950819,
+          "rotation": 4.71238898038469,
+          "target": "3-appartement---chambre"
+        },
+        {
+          "yaw": -2.1933800225112545,
+          "pitch": 0.19471555455463374,
+          "rotation": 1.5707963267948966,
+          "target": "4-appartement---salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-appartement---chambre",
+      "name": "Appartement - Chambre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1736293576432235,
+          "pitch": 0.1726300320868841,
+          "rotation": 0,
+          "target": "4-appartement---salon"
+        },
+        {
+          "yaw": -2.0218782577676855,
+          "pitch": 0.19267617392764436,
+          "rotation": 1.5707963267948966,
+          "target": "2-appartement---salle-de-bain"
+        },
+        {
+          "yaw": 1.1440338812124118,
+          "pitch": 0.20382163060060066,
+          "rotation": 0,
+          "target": "1-faade-derrier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-appartement---salon",
+      "name": "Appartement - Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18060323674290757,
+          "pitch": 0.17772710877105524,
+          "rotation": 0,
+          "target": "3-appartement---chambre"
+        },
+        {
+          "yaw": 0.11775178819023857,
+          "pitch": 0.02808562071337306,
+          "rotation": 4.71238898038469,
+          "target": "2-appartement---salle-de-bain"
+        },
+        {
+          "yaw": -2.799193890522929,
+          "pitch": 0.15548492956815174,
+          "rotation": 0,
+          "target": "0-faade-avant"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-garage",
+      "name": "Garage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4461700681550571,
+          "pitch": 0.02087667033366536,
+          "rotation": 0,
+          "target": "4-appartement---salon"
+        },
+        {
+          "yaw": -1.9603564514255876,
+          "pitch": 0.041083804244880895,
+          "rotation": 4.71238898038469,
+          "target": "0-faade-avant"
         }
       ],
       "infoHotspots": []
